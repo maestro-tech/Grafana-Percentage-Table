18bfe8dac4e00c14bc945a73f9c45fcf0c6e7449
Louis Ventre <louis.ventre@epitech.eu>
2017-07-27 10:55:12 +0200

    Review plugin arch

--- dist/module.js
+++ dist/module.js
@@ -1,39 +1,9 @@
 'use strict';
 
-System.register(['angular', 'lodash', 'jquery', 'moment', 'app/plugins/panel/table/module.js'], function (_export, _context) {
+System.register(['angular', 'lodash', 'jquery', 'moment', 'app/plugins/sdk', 'app/plugins/panel/table/module.js'], function (_export, _context) {
   "use strict";
 
-  var angular, _, $, moment, TablePanelCtrl, _createClass, _get, PercentageTablePanelCtrl;
-
-  function _classCallCheck(instance, Constructor) {
-    if (!(instance instanceof Constructor)) {
-      throw new TypeError("Cannot call a class as a function");
-    }
-  }
-
-  function _possibleConstructorReturn(self, call) {
-    if (!self) {
-      throw new ReferenceError("this hasn't been initialised - super() hasn't been called");
-    }
-
-    return call && (typeof call === "object" || typeof call === "function") ? call : self;
-  }
-
-  function _inherits(subClass, superClass) {
-    if (typeof superClass !== "function" && superClass !== null) {
-      throw new TypeError("Super expression must either be null or a function, not " + typeof superClass);
-    }
-
-    subClass.prototype = Object.create(superClass && superClass.prototype, {
-      constructor: {
-        value: subClass,
-        enumerable: false,
-        writable: true,
-        configurable: true
-      }
-    });
-    if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass;
-  }
+  var angular, _, $, moment, PanelCtrl, TablePanelCtrl;
 
   return {
     setters: [function (_angular) {
@@ -44,109 +14,61 @@ System.register(['angular', 'lodash', 'jquery', 'moment', 'app/plugins/panel/tab
       $ = _jquery.default;
     }, function (_moment) {
       moment = _moment.default;
+    }, function (_appPluginsSdk) {
+      PanelCtrl = _appPluginsSdk.PanelCtrl;
     }, function (_appPluginsPanelTableModuleJs) {
       TablePanelCtrl = _appPluginsPanelTableModuleJs.TablePanelCtrl;
     }],
     execute: function () {
-      _createClass = function () {
-        function defineProperties(target, props) {
-          for (var i = 0; i < props.length; i++) {
-            var descriptor = props[i];
-            descriptor.enumerable = descriptor.enumerable || false;
-            descriptor.configurable = true;
-            if ("value" in descriptor) descriptor.writable = true;
-            Object.defineProperty(target, descriptor.key, descriptor);
-          }
-        }
 
-        return function (Constructor, protoProps, staticProps) {
-          if (protoProps) defineProperties(Constructor.prototype, protoProps);
-          if (staticProps) defineProperties(Constructor, staticProps);
-          return Constructor;
-        };
-      }();
+      // class PercentageTablePanelCtrl extends PanelCtrl {
+      //
+      //   /** @ngInject */
+      //   constructor($scope, $injector, $rootScope) {
+      //     super($scope, $injector, $rootScope);
+      //     this.$rootScope = $rootScope;
+      //
+      //     _.defaults(this.panel, this.panelDefaults);
+      //   }
+      //
+      //   handleQueryResult(result) {
+      //     if (result.data.length !== 2) {""
+      //       let error = new Error();
+      //       error.message = 'Too many series error';
+      //       error.data = 'Metric query returns ' + result.data.length + ' series.\nPercentage stat table panel expects two series.';
+      //       throw error;
+      //     }
+      //
+      //     const lengthSerieA = result.data[0].rows.length;
+      //     const lengthSerieB = result.data[1].rows.length;
+      //     const maxLengthSeries = lengthSerieA > lengthSerieB ? lengthSerieA : lengthSerieB;
+      //
+      //     if (lengthSerieA !== lengthSerieB) {
+      //      let error = new Error();
+      //      error.message = 'Not same amount of data';
+      //      error.data = 'Serie A has ' + lengthSerieA + ' while Serie B has ' + lengthSerieB;
+      //      throw error;
+      //     }
+      //
+      //     for (let i = 0; i < maxLengthSeries; i++) {
+      //       const dataFirst = result.data[0].rows[i][2];
+      //       const dataSecond = result.data[1].rows[i][2];
+      //       const res = (dataFirst / dataSecond) * 100;
+      //       result.data[0].rows[i][2] = res;
+      //     }
+      //     return super.handleQueryResult(result);
+      //   }
+      // }
+      //
+      // PercentageTablePanelCtrl.templateUrl = 'module.html';
 
-      _get = function get(object, property, receiver) {
-        if (object === null) object = Function.prototype;
-        var desc = Object.getOwnPropertyDescriptor(object, property);
+      ///<reference path="../../../headers/common.d.ts" />
 
-        if (desc === undefined) {
-          var parent = Object.getPrototypeOf(object);
+      console.log(TablePanelCtrl.prototype.__proto__);
 
-          if (parent === null) {
-            return undefined;
-          } else {
-            return get(parent, property, receiver);
-          }
-        } else if ("value" in desc) {
-          return desc.value;
-        } else {
-          var getter = desc.get;
+      _export('TablePanelCtrl', TablePanelCtrl);
 
-          if (getter === undefined) {
-            return undefined;
-          }
-
-          return getter.call(receiver);
-        }
-      };
-
-      _export('PanelCtrl', _export('PercentageTablePanelCtrl', PercentageTablePanelCtrl = function (_TablePanelCtrl) {
-        _inherits(PercentageTablePanelCtrl, _TablePanelCtrl);
-
-        /** @ngInject */
-        function PercentageTablePanelCtrl($scope, $injector, $rootScope) {
-          _classCallCheck(this, PercentageTablePanelCtrl);
-
-          var _this = _possibleConstructorReturn(this, (PercentageTablePanelCtrl.__proto__ || Object.getPrototypeOf(PercentageTablePanelCtrl)).call(this, $scope, $injector, $rootScope));
-
-          _this.$rootScope = $rootScope;
-
-          _.defaults(_this.panel, _this.panelDefaults);
-          return _this;
-        }
-
-        _createClass(PercentageTablePanelCtrl, [{
-          key: 'handleQueryResult',
-          value: function handleQueryResult(result) {
-            if (result.data.length !== 2) {
-              var error = new Error();
-              error.message = 'Too many series error';
-              error.data = 'Metric query returns ' + result.data.length + ' series.\nPercentage stat table panel expects two series.';
-              throw error;
-            }
-
-            var lengthSerieA = result.data[0].rows.length;
-            var lengthSerieB = result.data[1].rows.length;
-            var maxLengthSeries = lengthSerieA > lengthSerieB ? lengthSerieA : lengthSerieB;
-
-            if (lengthSerieA !== lengthSerieB) {
-              var _error = new Error();
-              _error.message = 'Not same amount of data';
-              _error.data = 'Serie A has ' + lengthSerieA + ' while Serie B has ' + lengthSerieB;
-              throw _error;
-            }
-
-            for (var i = 0; i < maxLengthSeries; i++) {
-              var dataFirst = result.data[0].rows[i][2];
-              var dataSecond = result.data[1].rows[i][2];
-              var res = dataFirst / dataSecond * 100;
-              result.data[0].rows[i][2] = res;
-            }
-
-            console.log('calling');
-            return _get(PercentageTablePanelCtrl.prototype.__proto__ || Object.getPrototypeOf(PercentageTablePanelCtrl.prototype), 'handleQueryResult', this).call(this, result);
-          }
-        }]);
-
-        return PercentageTablePanelCtrl;
-      }(TablePanelCtrl)));
-
-      PercentageTablePanelCtrl.templateUrl = 'public/app/plugins/panel/table/module.html';
-
-      _export('PercentageTablePanelCtrl', PercentageTablePanelCtrl);
-
-      _export('PanelCtrl', PercentageTablePanelCtrl);
+      _export('PanelCtrl', TablePanelCtrl);
     }
   };
 });
--- src/module.js
+++ src/module.js
@@ -4,52 +4,53 @@ import angular from 'angular';
 import _ from 'lodash';
 import $ from 'jquery';
 import moment from 'moment';
+import {PanelCtrl} from 'app/plugins/sdk';
 import {TablePanelCtrl} from 'app/plugins/panel/table/module.js';
 
-class PercentageTablePanelCtrl extends TablePanelCtrl {
+// class PercentageTablePanelCtrl extends PanelCtrl {
+//
+//   /** @ngInject */
+//   constructor($scope, $injector, $rootScope) {
+//     super($scope, $injector, $rootScope);
+//     this.$rootScope = $rootScope;
+//
+//     _.defaults(this.panel, this.panelDefaults);
+//   }
+//
+//   handleQueryResult(result) {
+//     if (result.data.length !== 2) {""
+//       let error = new Error();
+//       error.message = 'Too many series error';
+//       error.data = 'Metric query returns ' + result.data.length + ' series.\nPercentage stat table panel expects two series.';
+//       throw error;
+//     }
+//
+//     const lengthSerieA = result.data[0].rows.length;
+//     const lengthSerieB = result.data[1].rows.length;
+//     const maxLengthSeries = lengthSerieA > lengthSerieB ? lengthSerieA : lengthSerieB;
+//
+//     if (lengthSerieA !== lengthSerieB) {
+//      let error = new Error();
+//      error.message = 'Not same amount of data';
+//      error.data = 'Serie A has ' + lengthSerieA + ' while Serie B has ' + lengthSerieB;
+//      throw error;
+//     }
+//
+//     for (let i = 0; i < maxLengthSeries; i++) {
+//       const dataFirst = result.data[0].rows[i][2];
+//       const dataSecond = result.data[1].rows[i][2];
+//       const res = (dataFirst / dataSecond) * 100;
+//       result.data[0].rows[i][2] = res;
+//     }
+//     return super.handleQueryResult(result);
+//   }
+// }
+//
+// PercentageTablePanelCtrl.templateUrl = 'module.html';
 
-  /** @ngInject */
-  constructor($scope, $injector, $rootScope) {
-    super($scope, $injector, $rootScope);
-    this.$rootScope = $rootScope;
-
-    _.defaults(this.panel, this.panelDefaults);
-  }
-
-  handleQueryResult(result) {
-    if (result.data.length !== 2) {
-      let error = new Error();
-      error.message = 'Too many series error';
-      error.data = 'Metric query returns ' + result.data.length + ' series.\nPercentage stat table panel expects two series.';
-      throw error;
-    }
-
-    const lengthSerieA = result.data[0].rows.length;
-    const lengthSerieB = result.data[1].rows.length;
-    const maxLengthSeries = lengthSerieA > lengthSerieB ? lengthSerieA : lengthSerieB;
-
-    if (lengthSerieA !== lengthSerieB) {
-     let error = new Error();
-     error.message = 'Not same amount of data';
-     error.data = 'Serie A has ' + lengthSerieA + ' while Serie B has ' + lengthSerieB;
-     throw error;
-    }
-
-    for (let i = 0; i < maxLengthSeries; i++) {
-      const dataFirst = result.data[0].rows[i][2];
-      const dataSecond = result.data[1].rows[i][2];
-      const res = (dataFirst / dataSecond) * 100;
-      result.data[0].rows[i][2] = res;
-    }
-
-    console.log('calling');
-    return super.handleQueryResult(result);
-  }
-}
-
-PercentageTablePanelCtrl.templateUrl = 'public/app/plugins/panel/table/module.html';
+console.log(TablePanelCtrl.prototype.__proto__);
 
 export {
-  PercentageTablePanelCtrl,
-  PercentageTablePanelCtrl as PanelCtrl
+  TablePanelCtrl,
+  TablePanelCtrl as PanelCtrl
 };
